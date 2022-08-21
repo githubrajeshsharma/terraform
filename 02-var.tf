c34fb72c1d43d11aad64f180969b2385e7ec1c21
rajesh <rajesh.shahya@gmail.com>
2022-08-21 17:04:46 +0530

    approve

--- 02-var.tf
+++ 02-var.tf
@@ -22,35 +22,35 @@ variable "example-list" {
     ]
 }
 
-# # Printing a list variable 
-# output "ex-list" {
-#     value = "Welcome to ${var.example-list[0]} Training, Trainer name is ${var.example-list[3]}, Training duration is ${var.example-list[1]} hours"
-# }
+# Printing a list variable 
+output "ex-list" {
+    value = "Welcome to ${var.example-list[0]} Training, Trainer name is ${var.example-list[3]}, Training duration is ${var.example-list[1]} hours"
+}
 
-# # Declaring a map variable
-# variable "example-map" {
-#     default = {
-#         Class    = "DevOps",
-#         Timings  = "0730AMIST",
-#         Duration = 85
-#     }
-# }
+# Declaring a map variable
+variable "example-map" {
+    default = {
+        Class    = "DevOps",
+        Timings  = "0730AMIST",
+        Duration = 85
+    }
+}
 
-# # output "ex-map" {
-# #     value = "Welcome to ${var.example-map["Class"]} Training and the training timings are ${var.example-map["Timings"]} and the duration of the over all training is ${var.example-map["Duration"]} "
-# # }
+ output "ex-map" {
+     value = "Welcome to ${var.example-map["Class"]} Training and the training timings are ${var.example-map["Timings"]} and the duration of the over all training is ${var.example-map["Duration"]} "
+ }
 
 
-# # ###############
+ ###############
 
-# # variable "state" {}
+ variable "state" {}
 
-# # output "state-name" {
-# #     value = "Name of the state where trainer resides is ${var.state}"
-# }
+ output "state-name" {
+     value = "Name of the state where trainer resides is ${var.state}"
+}
 
-# variable "country" {}
+variable "country" {}
 
-# output "country-name" {
-#     value = "Name of the country where trainer resides is ${var.country}"
-# }
+output "country-name" {
+    value = "Name of the country where trainer resides is ${var.country}"
+}
