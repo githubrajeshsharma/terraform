262fba9c65a918cde0990f80fd5f24529802ea25
rajesh <rajesh.shahya@gmail.com>
2022-08-21 18:04:43 +0530

    approve

--- ec2 instance/ec2.tf
+++ ec2 instance/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
   ami           = i-099a9f588ad15acf2
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "my first ec2 instance_type"
