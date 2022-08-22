0a52e689214c9bc6c759a99db3d0dbe3d6a1a9c2
rajesh <rajesh.shahya@gmail.com>
2022-08-22 21:02:14 +0530

    approve

--- ec2 instance/ec2.tf
+++ ec2 instance/ec2.tf
@@ -1,13 +1,41 @@
 resource "aws_instance" "sample" {
   ami           = "ami-0aa718de62aea6fbe"
   instance_type = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "my first ec2 instance_type"
-  }
+    ENV  = "Dev"
+    Project = "cc"
+   }
 }
 
 output "private_ip" {
 value  = "aws_instance.sample.private_ip"
 }
 
+# # Creating security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH to VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
